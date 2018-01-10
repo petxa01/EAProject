0d2d47e96484114ab1e9681a708810fbd2161f12
NetShow7 <netshow.ep@gmail.com>
2018-01-10 09:31:17 +0100

    Menú a medias y clase Read

--- src/EA/Main.java
+++ src/EA/Main.java
@@ -1,69 +1,112 @@
 package EA;
 
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // write your code here
+        boolean repeat;
+        int chooser;
         //Main menu
-        System.out.println("[1] Games");
-        System.out.println("[2] Franchises");
-        System.out.println("[3] Studios");
-        System.out.println("---------------");
-        System.out.println("[4] Developers");
-        System.out.println("---------------");
-        System.out.println("[5] Clients");
-        System.out.println("---------------");
-        System.out.println("[6] Divisions");
-        System.out.println("---------------");
-        System.out.println("[0] Exit");
+        do {
+            repeat = true;
+            Runtime.getRuntime().exec("cls");
+            System.out.println("+++++++++++++++++++++++++++");
+            System.out.println("+       [1] Games         +");
+            System.out.println("+    [2] Franchises       +");
+            System.out.println("+      [3] Studios        +");
+            System.out.println("+-------------------------+");
+            System.out.println("+     [4] Developers      +");
+            System.out.println("+-------------------------+");
+            System.out.println("+      [5] Clients        +");
+            System.out.println("+-------------------------+");
+            System.out.println("+     [6] Divisions       +");
+            System.out.println("+-------------------------+");
+            System.out.println("+       [0] Exit          +");
+            System.out.println("+++++++++++++++++++++++++++");
+            chooser = Read.Int();
+            switch (chooser) {
+                case 1:
+                    //Games
+                    System.out.println("+++++++++++++++++++++++++++");
+                    System.out.println("+      [1] Game List      +");
+                    System.out.println("+       [2] Search        +");
+                    System.out.println("+      [3] Add Game       +");
+                    System.out.println("+     [4] Delete Game     +");
+                    System.out.println("+      [5] Edit Game      +");
+                    System.out.println("+-------------------------+");
+                    System.out.println("+        [0] Back         +");
+                    System.out.println("+++++++++++++++++++++++++++");
+                    break;
+                case 2:
+                    //Franchises
+                    System.out.println("+++++++++++++++++++++++++++++++++++");
+                    System.out.println("+        [1] Franchise List       +");
+                    System.out.println("+---------------");
+                    System.out.println("+[2] Add Franchise");
+                    System.out.println("+[3] Delete Franchise");
+                    System.out.println("+---------------");
+                    System.out.println("+[4] Add Game to Franchise");
+                    System.out.println("+[5] Delete Game from Franchise");
+                    System.out.println("+---------------");
+                    System.out.println("+[6] Edit Franchise");
+                    System.out.println("+---------------");
+                    System.out.println("+[0] Back");
+                    System.out.println("++++++++++++++++++++++++++++++++++");
+                    break;
+                case 3:
+                    //Studios
+                    System.out.println("[1] Studio List");
+                    System.out.println("---------------");
+                    System.out.println("[2] Add Studio");
+                    System.out.println("[3] Delete Studio");
+                    System.out.println("---------------");
+                    System.out.println("[4] Add Franchise to Studio");
+                    System.out.println("[5] Delete Franchise from Studio");
+                    System.out.println("---------------");
+                    System.out.println("[6] Edit Studio");
+                    System.out.println("---------------");
+                    System.out.println("[0] Back");
+                    break;
+                case 4:
+                    //Developers
+                    System.out.println("[1] Dev List");
+                    System.out.println("[2] Search");
+                    System.out.println("[3] Add Developer");
+                    System.out.println("[4] Delete Developer");
+                    System.out.println("[5] Edit Developer");
+                    System.out.println("---------------");
+                    System.out.println("[6] Add Game to Dev");
+                    System.out.println("[7] Remove Game from Dev");
+                    System.out.println("---------------");
+                    System.out.println("[8] List of Developing Games");
+                    System.out.println("---------------");
+                    System.out.println("[0] Back");
+                    break;
+                case 5:
+                    //Clients
+                    System.out.println("[1] Client list");
+                    System.out.println("[2] Add new client");
+                    System.out.println("[3] Delete client");
+                    System.out.println("[4] Edit client");
+                    System.out.println("---------------");
+                    System.out.println("[0] Back");
+                    break;
+                case 6:
+                    //Divisions
+                    System.out.println("[1] Division list");
+                    System.out.println("[2] Add division");
+                    System.out.println("[3] Delete division");
+                    System.out.println("[4] Edit division");
+                    System.out.println("---------------");
+                    System.out.println("[0] Back");
+                    break;
+                case 0:
+                    repeat = false;
 
-        //Games
-        System.out.println("[1] Game List");
-        System.out.println("[2] Search");
-        System.out.println("[3] Add Game");
-        System.out.println("[4] Delete Game");
-        System.out.println("[5] Edit Game");
-        System.out.println("---------------");
-        System.out.println("[0] Back");
-        //Franchises
-        System.out.println("[1] Franchise List");
-        System.out.println("---------------");
-        System.out.println("[2] Add Franchise");
-        System.out.println("[3] Delete Franchise");
-        System.out.println("---------------");
-        System.out.println("[4] Add Game to Franchise");
-        System.out.println("[5] Delete Game from Franchise");
-        System.out.println("---------------");
-        System.out.println("[6] Edit Franchise");
-        System.out.println("---------------");
-        System.out.println("[0] Back");
-        //Studios
-        System.out.println("[1] Studio List");
-        System.out.println("---------------");
-        System.out.println("[2] Add Studio");
-        System.out.println("[3] Delete Studio");
-        System.out.println("---------------");
-        System.out.println("[4] Add Franchise to Studio");
-        System.out.println("[5] Delete Franchise from Studio");
-        System.out.println("---------------");
-        System.out.println("[6] Edit Studio");
-        System.out.println("---------------");
-        System.out.println("[0] Back");
-        //Developers
-        System.out.println("[1] Dev List");
-        System.out.println("[2] Search");
-        System.out.println("[3] Add Developer");
-        System.out.println("[4] Delete Developer");
-        System.out.println("[5] Edit Developer");
-        System.out.println("---------------");
-        System.out.println("[6] Add Game to Dev");
-        System.out.println("[7] Remove Game from Dev");
-        System.out.println("---------------");
-        System.out.println("[8] List of Developing Games");
-        System.out.println("---------------");
-        System.out.println("[0] Back");
-        //Clients
+            }
+        } while (repeat);
 
-        //Divisions
     }
 }
---
+++ src/EA/Read.java
@@ -0,0 +1,93 @@
+package EA;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * @author DM3-1-20
+ */
+public class Read {
+//    public static Object object(){
+//        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+//        String s = null;
+//        Object a;
+//        try {
+//            s = reader.readLine();
+//            a = String.toObject();
+//        } catch (IOException ex) {
+//            Logger.getLogger(read.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//    }
+
+    public static int Int() {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String s = null;
+        int a = 0;
+        boolean x;
+        do {
+            x = false;
+            try {
+                s = reader.readLine();
+                a = Integer.parseInt(s);
+            } catch (Exception Integer) {
+                System.out.println("You typed something wrong, try again");
+                System.out.println("");
+                x = true;
+            }
+        } while (x == true);
+        return a;
+    }
+
+    public static float Float() {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String s = null;
+        float a = 0;
+        boolean x;
+        do {
+            x = false;
+            try {
+                s = reader.readLine();
+                a = Float.parseFloat(s);
+            } catch (Exception Float) {
+                System.out.println("You typed something wrong, try again");
+                System.out.println("");
+                x = true;
+            }
+        } while (x == true);
+        return a;
+    }
+
+    public static double Double() {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String s = null;
+        double a = 0;
+        boolean x;
+        do {
+            x = false;
+            try {
+                s = reader.readLine();
+                a = Double.parseDouble(s);
+            } catch (Exception Double) {
+                System.out.println("You typed something wrong, try again");
+                System.out.println("");
+                x = true;
+            }
+        } while (x == true);
+        return a;
+    }
+
+    public static String String() {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String a = null;
+        try {
+            a = reader.readLine();
+        } catch (IOException ex) {
+            Logger.getLogger(Read.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return a;
+    }
+}
