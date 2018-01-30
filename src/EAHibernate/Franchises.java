94931513a035ed379a7068392864ccfe226e5a3a
petxa01 <petxa01@gmail.com>
2018-01-30 19:30:14 +0100

    a

--- src/EAHibernate/Franchises.java
+++ src/EAHibernate/Franchises.java
@@ -1,8 +1,14 @@
 package EAHibernate;
 
+import EA.Read;
+import org.hibernate.*;
+import org.hibernate.query.Query;
+
 import javax.persistence.*;
-import java.util.Collection;
-import java.util.Objects;
+import java.sql.SQLException;
+import java.util.*;
+
+import static EAHibernate.HibernateTools.getSessionFactory;
 
 @Entity
 public class Franchises {
@@ -75,5 +81,68 @@ public class Franchises {
 
     public void setGamesById(Collection<Games> gamesById) {
         this.gamesById = gamesById;
+    }
+
+    public static void insertFranchises(int idFranchise, String nombre, int studioId) {
+        SessionFactory sf = getSessionFactory();
+        Session sesioa = sf.openSession();
+        Transaction tx = sesioa.beginTransaction();
+        Franchises f = new Franchises();
+        f.setId(idFranchise);
+        f.setName(nombre);
+        f.setStudioId(studioId);
+        sesioa.save(f);
+        tx.commit();
+
+    }
+
+    public static void selectFranchises() throws ClassNotFoundException, SQLException {
+        Session ses;
+
+        try {
+            SessionFactory sf = getSessionFactory();
+            ses = sf.openSession();
+            String e = "from Franchises";
+            Query q = ses.createQuery(e);
+            List results = q.list();
+            Iterator iterator = results.iterator();
+            System.out.println("Name\t\t\t\tStudio ID");
+            while (iterator.hasNext()) {
+                Franchises fr = (Franchises) iterator.next();
+                System.out.println(fr.getName() + "\t\t\t\t" + fr.getStudioId());
+            }
+        } catch (HibernateException ex) {
+            System.out.println("Exception ocurred: " + ex.getMessage());
+        }
+
+    }
+    void updatefranchises(Integer newId, String newName, Integer newStudioId) {
+        SessionFactory sf = getSessionFactory();
+        Session sesion = sf.openSession();
+        Transaction tx = sesion.beginTransaction();
+        int id= Read.Int("Enter the Id of the franchise you want to update");
+        Franchises fr = new Franchises();
+        fr.setStudioId(newId);
+        fr.setName(newName);
+        fr.setStudioId(newStudioId);
+
+        sesion.update(fr);
+        tx.commit();
+        sesion.close();
+        sf.close();
+
+    }
+    public static void deleteFranchises(int idPueblos) {
+        SessionFactory sf = getSessionFactory();
+        Session sesion = sf.openSession();
+        Transaction tx = sesion.beginTransaction();
+
+        Pueblos objeto_pueblos = new Pueblos(idPueblos);
+        sesion.delete(objeto_pueblos);//delete egiten du
+        System.out.println("Erregistroa ezabatua izan da");
+
+        tx.commit();
+        sesion.close();
+        //sesionfact.close();
     }
 }
